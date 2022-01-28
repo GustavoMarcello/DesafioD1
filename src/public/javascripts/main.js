ab1d609b2c9a19130300f4851b6369da33e35ed5
Gustavo Marcello <gustavo.marcello@compasso.com.br>
2022-01-28 20:59:24 -0300

    rasa

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,13 +1,37 @@
 const url = "http://localhost:3000/api/v1/popular_movies"
 
-function getPopular(){
-    axios.get(url)
-        .then(response =>{
-            const data = response.data
 
-            renderApiResult.textContent = JSON.stringify(data)
-        })
-        .catch(error => console.log(error))
+axios.get(url)
+    .then(response =>{
+        const data = response.data
+        // renderApiResult.textContent = JSON.stringify(data)
+    })
+    .catch(error => console.log(error))
+
+
+const filmes = [
+
+]
+
+const dFrag = document.createDocumentFragment();
+
+filmes.map(t => {
+    const div = document.createElement('div')
+    // const filmeName = document.createElement('span')
+    const p = document.createElement('img')
+    // p.textContent = t.img
+    // filmeName.textContent = t.nome
+    // filmeName.setAttribute('class', 'tooltiptext')
+    p.setAttribute('src', t.img)
+    p.setAttribute('alt', t.nome)
+    p.setAttribute('class', 'imagem')
+    div.setAttribute('class', 'nova div')
+    div.appendChild(p)
+    
+    dFrag.appendChild(div);
+})
+
+window.onload = () => {
+    //write your code here
+    document.getElementById('carousel').appendChild(dFrag);
 }
-
-getPopular()
