28217850cc79f27566be43a4382d077f35c3c4d8
caio-gferreira <caiogfferreira@gmail.com>
2022-01-30 23:34:16 -0300

    fixing scroll: carousel

--- src/public/javascripts/main.js
+++ src/public/javascripts/main.js
@@ -1,74 +1,112 @@
-const popularMovies = "http://localhost:3000/api/v1/popular_movies"
-const upcommingMovies = "http://localhost:3000/api/v1/upcoming_movies"
-const topMovies = "http://localhost:3000/api/v1/top_movies"
 const slider0 = document.querySelector(".slider0");
 const slider1 = document.querySelector(".slider1");
 const slider2 = document.querySelector(".slider2");
+const indicator0 = document.querySelectorAll(".indicator");
+const indicator1 = document.querySelectorAll(".indicator1");
+const indicator2 = document.querySelectorAll(".indicator2");
+const btnLeft0 = document.getElementById("moveLeft0");
+const btnRight0 = document.getElementById("moveRight0");
+const btnLeft1 = document.getElementById("moveLeft1");
+const btnRight1 = document.getElementById("moveRight1");
+const btnLeft2 = document.getElementById("moveLeft2");
+const btnRight2 = document.getElementById("moveRight2");
 
-function populateSlider(movies) {
-    movies.forEach((image) => {
-      // Clone the initial movie thats included in the html, then replace the image with a different one
-      const newMovie = document.getElementById("movie0");
-      const clone = newMovie.cloneNode(true);
-      const img = clone.querySelector("img");
-      img.src = image.poster_path;
-  
-      slider0.insertBefore(clone, slider0.childNodes[slider0.childNodes.length - 1]);
+const movies = [
+    {url: 'http://localhost:3000/api/v1/popular_movies', slider: slider0, id: 'movie0', movieClass: '.movie0', indicator: indicator0, btnRight: btnRight0, btnLeft: btnLeft0},
+    {url: 'http://localhost:3000/api/v1/upcoming_movies', slider: slider1, id: 'movie1', movieClass: '.movie1', indicator: indicator1, btnRight: btnRight1, btnLeft: btnLeft1},
+    {url: 'http://localhost:3000/api/v1/top_movies', slider: slider2, id: 'movie2', movieClass: '.movie2', indicator: indicator2, btnRight: btnRight2, btnLeft: btnLeft2}
+]
+// Update the indicators that show which page we're currently on
+function updateIndicators(index) {
+    for(let indicators of movies) {
+        indicators.indicator.forEach((indicator) => {
+            indicator.classList.remove("active");
+          });
+          let newActiveIndicator = indicators.indicator[index];
+          newActiveIndicator.classList.add("active");
+    }
+    
+  }
+class CarouselMovies {
+    /**
+     * 
+     * @param {String} slider 
+     * @param {String} id 
+     */
+    carouselSlider(slider, id, data) {
+        data.forEach((image) => {
+        // Clone the initial movie thats included in the html, then replace the image with a different one
+            const newMovie = document.getElementById(id);
+            const clone = newMovie.cloneNode(true);
+            const img = clone.querySelector("img");
+            img.src = image.poster_path;
+        
+            slider.insertBefore(clone, slider.childNodes[slider.childNodes.length - 1]);
+        });
+    }
+}
+
+const carouselMovies = new CarouselMovies()
+
+let activeIndex = 0; // the current page on the slider
+movies.map(x => {
+    
+    axios.get(x.url)
+        .then(response =>{
+            const data = response.data
+                carouselMovies.carouselSlider(x.slider, x.id, data)
+                carouselMovies.carouselSlider(x.slider, x.id, data)
+                const initialMovie = document.getElementById(x.id);
+                initialMovie.remove();  
+            })
+        .catch(error => console.log(error))
+})
+
+movies.map(x => {
+    x.btnLeft.addEventListener("click", (e) => {
+        let movieWidth = document.querySelector(x.movieClass).getBoundingClientRect()
+          .width;
+        let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
+      
+        x.slider.scrollBy({
+          top: 0,
+          left: -scrollDistance,
+          behavior: "smooth",
+        });
+        activeIndex = (activeIndex - 1) % 3;
+        
+        updateIndicators(activeIndex);
     });
-}
-
-function upcomingSlider(movies) {
-    movies.forEach((image) => {
-        // Clone the initial movie thats included in the html, then replace the image with a different one
-        const newMovie = document.getElementById("movie1");
-        const clone = newMovie.cloneNode(true);
-        const img = clone.querySelector("img");
-        img.src = image.poster_path;
     
-        slider1.insertBefore(clone, slider1.childNodes[slider1.childNodes.length - 1]);
-      });
-}
-function topSlider(movies) {
-    movies.forEach((image) => {
-        // Clone the initial movie thats included in the html, then replace the image with a different one
-        const newMovie = document.getElementById("movie2");
-        const clone = newMovie.cloneNode(true);
-        const img = clone.querySelector("img");
-        img.src = image.poster_path;
+      // Scroll Right button
+    x.btnRight.addEventListener("click", (e) => {
+        
+        let movieWidth = document.querySelector(x.movieClass).getBoundingClientRect()
+            .width;
+        let scrollDistance = movieWidth * 6; // Scroll the length of 6 movies. TODO: make work for mobile because (4 movies/page instead of 6)
     
-        slider2.insertBefore(clone, slider2.childNodes[slider2.childNodes.length - 1]);
-      });
-}
+        // if we're on the last page
+        if (activeIndex == 3) {
+        // duplicate all the items in the slider (this is how we make 'looping' slider)
+            x.slider.scrollBy({
+                top: 0,
+                left: +scrollDistance,
+                behavior: "smooth",
+            });
+            activeIndex = 0;
+            updateIndicators(activeIndex);
+        } else {
+            x.slider.scrollBy({
+                top: 0,
+                left: +scrollDistance,
+                behavior: "smooth",
+        });
+        activeIndex = (activeIndex + 1) % 3;
+        console.log(activeIndex);
+        updateIndicators(activeIndex);
+        }
+    });
+})
 
-axios.get(popularMovies)
-    .then(response =>{
-        const data = response.data
-        console.log(data)
-        populateSlider(data)
-        const initialMovie = document.getElementById("movie0");
-        initialMovie.remove();
-        
-    })
-    .catch(error => console.log(error))
 
-axios.get(upcommingMovies)
-    .then(response =>{
-        const data = response.data
-        console.log(data)
-        upcomingSlider(data)
-        const initialMovie = document.getElementById("movie1");
-        initialMovie.remove();
-        
-    })
-    .catch(error => console.log(error))
-
-axios.get(topMovies)
-    .then(response =>{
-        const data = response.data
-        console.log(data)
-        topSlider(data)
-        const initialMovie = document.getElementById("movie2");
-        initialMovie.remove();
-        
-    })
-    .catch(error => console.log(error))
+      
